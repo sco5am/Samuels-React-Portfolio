eb0dd61e7db98282e4f8bd90edb9e70edd834b82
Sam Oaks <samuelclayoaks@yahoo.com>
2023-04-11 18:57:28 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import HelloReact from './components/HelloReact';
 
-// TODO: Add a comment explaining what this function is doing
-// This function is a functional component that helps us split the UI into distinct parts.
-// In this case, we are returning another component, <HelloReact/> from it.
 function App() {
   return <HelloReact />;
 }
