ed8ec5d1dd9e37a8d9026f741ba286ed8bb98722
Sam Oaks <samuelclayoaks@yahoo.com>
2023-04-14 18:19:55 -0400

    added project funtion to accept props from Portfolio.js

---
+++ src/components/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <p className="content is-medium"> Full Stack Web Developer</p>
+      <hr />
+      <img
+        className="my-pic"
+        src={process.env.PUBLIC_URL + "/img/profile.jpg"}
+        alt="Samuel Clay Oaks"
+      />
+      <p className="content is-italic mt-4">
+        Hi I'm Sam, and welcome to my portfolio! I'm a student in The Ohio State
+        Full Stack Development bootcamp.
+      </p>
+      <p className="content">
+        I've created this site in order to showcase the projects ive completed
+        with my ever expanding skills using technologies such as HTML, CSS,
+        Javascript, React, etc.
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>My Full Stack Development Portfolio</strong> - a React site by{" "}
+          <a href="https://github.com/sco5am" target="_blank" rel="noreferrer">
+            Samuel Clay Oaks
+          </a>
+          .
+          <hr />
+          <a
+            href="https://www.linkedin.com/in/samuel-oaks-429451272/"
+            target="_blank" rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
